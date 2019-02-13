daa2e8ecc489c08081436bbf0e181369661bac14
Shaurya Trehan <35552833+strehan321@users.noreply.github.com>
2019-02-13 15:06:56 -0600

    Update TableVarsSetOps.sql

--- SQl Script/TableVarsSetOps.sql
+++ SQl Script/TableVarsSetOps.sql
@@ -1,7 +1,5 @@
 /**************************************************************
   TABLE VARIABLES AND SET OPERATORS
-  Works for SQLite, Postgres
-  MySQL doesn't support Intersect or Except
 **************************************************************/
 
 /**************************************************************
@@ -21,18 +19,6 @@ where A.sID = S.sID and A.cName = C.cName;
 /**************************************************************
   Pairs of students with same GPA
 **************************************************************/
-
-select S1.sID, S1.sName, S1.GPA, S2.sID, S2.sName, S2.GPA
-from Student S1, Student S2
-where S1.GPA = S2.GPA;
-
-/*** Get rid of self-pairings ***/
-
-select S1.sID, S1.sName, S1.GPA, S2.sID, S2.sName, S2.GPA
-from Student S1, Student S2
-where S1.GPA = S2.GPA and S1.sID <> S2.sID;
-
-/*** Get rid of reverse-pairings ***/
 
 select S1.sID, S1.sName, S1.GPA, S2.sID, S2.sName, S2.GPA
 from Student S1, Student S2
@@ -58,7 +44,7 @@ select cName as name from College
 union all
 select sName as name from Student;
 
-/*** Notice not sorted any more (SQLite), add order by cName ***/
+/*** Order by cName ***/
 
 select cName as name from College
 union all
@@ -75,14 +61,12 @@ select sID from Apply where major = 'EE';
 
 /**************************************************************
   IDs of students who applied to both CS and EE
-  Some systems don't support intersect
 **************************************************************/
 
 select A1.sID
 from Apply A1, Apply A2
 where A1.sID = A2.sID and A1.major = 'CS' and A2.major = 'EE';
 
-/*** Why so many duplicates? Look at Apply table ***/
 /*** Add Distinct ***/
 
 select distinct A1.sID
@@ -99,7 +83,6 @@ select sID from Apply where major = 'EE';
 
 /**************************************************************
   IDs of students who applied to CS but not EE
-  Some systems don't support except
 **************************************************************/
 
 select A1.sID
@@ -111,5 +94,3 @@ where A1.sID = A2.sID and A1.major = 'CS' and A2.major <> 'EE';
 select distinct A1.sID
 from Apply A1, Apply A2
 where A1.sID = A2.sID and A1.major = 'CS' and A2.major <> 'EE';
-
-/*** Can't do it with constructs we have so far ***/
